9f56b4e952b73a3c16cdd1624e810c9f69f40a6c
이영훈 <57934100+zerohoon96@users.noreply.github.com>
2021-04-08 03:00:35 +0900

    Update pyupbit_test.py
    후행스팬 추가

--- src/pyupbit_test.py
+++ src/pyupbit_test.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import pyupbit
 import datetime
 import time
@@ -56,15 +57,27 @@ def sell_crypto_currency(coin):
     unit = upbit.get_balance(coin)
     upbit.sell_market_order(coin, unit)
 
-##########################################
+#후행스팬 값을 얻는 함수
+def get_lagging_span(coin,min=26):
+    prio=now-datetime.timedelta(minutes=min)
+    prio=prio.strftime('%Y-%m-%d %H:%M:00')
+    prio=pd.to_datetime(prio)
+    test_query={'time':{'$in':[prio]}}
+    ret=client['younghoon_test'][coin].find_one(test_query, {"_id": False})
+    if ret==None:
+        return -1
+    else:
+        print("후행스팬 : ",prio," ",ret['close']," 값과 비교")
+        return ret['close']
+
 #초기 타겟 가격 저장 (안하면 정각에 첫 데이터 저장될때까지 기다려야함..)
 for coin in coins:
-            res = get_target_price(coin)
-            #목표가 dictionary에 저장
-            target_price[coin]=res
-            print(coin, '\t\ttarget price : ',res)
-            time.sleep(0.2)
-##########################################
+    res = get_target_price(coin)
+    #목표가 dictionary에 저장
+    target_price[coin]=res
+    print(coin, '\t\ttarget price : ',res)
+    time.sleep(0.2)
+
 now = datetime.datetime.now()
 mid = datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
 while True:
@@ -85,7 +98,8 @@ while True:
     krw=upbit.get_balance('KRW')
     #모든 종목에 대해서 디비에 데이터를 저장하며 매수 검사(가격이 높을때에만) → 다중 쓰레드를 써야할듯. 1분안에 안끝나
     for coin in coins:
-        print('------------------------------------------------------------------------------------------------------------------------------')
+        now = datetime.datetime.now()
+        print('-----------------------------------------')
         print(coin," 종목 테스트")
         #20개 캔들봉을 받아와서 dictionary list로 변경. 마지막 데이터는 변동중이므로 저장하지 않음
         items_df = pyupbit.get_ohlcv(ticker=coin, interval="minute1", count=20)
@@ -95,10 +109,11 @@ while True:
         #19개의 캔들봉 데이터를 중복검사 후에 저장
         tmp = [items_df.loc[i,:].to_dict() for i in range(len(items_df)-1)]
         items=[]
+
         for i in tmp:
             query = {'time':{'$in':[i['time']]}}
             ret=client['younghoon_test'][coin].find_one(query, {"_id": False})
-            #중복이면 제거
+            #중복이면 insert하지 않음
             if ret == None:
                 items.append(i)
         for i in items:
@@ -113,11 +128,15 @@ while True:
         print('5분평균 : ',avg_5)
         print('10분평균 : ',avg_10)
         print('20분평균 : ',avg_20)
-        
-        #골든크로스 상태이고 목표가보다 가격이 높으면 매수 시도
-        if avg_5> avg_10 and avg_5 > avg_20:
+        #골든크로스 검사
+        if avg_5 > avg_10 and avg_5 > avg_20:
+            lag_span=get_lagging_span(coin,min=26)
             price = pyupbit.get_current_price(coin)
-            if price > target_price[coin]:
-                buy_crypto_currency(coin)
-                krw=upbit.get_balance('KRW')
+            #후행스팬 검사
+            if lag_span > 0 and price > lag_span:
+                #목표가 도달 검사
+                if price > target_price[coin]:
+                    #매수 실행
+                    buy_crypto_currency(coin)
+                    krw=upbit.get_balance('KRW')
         time.sleep(0.2)
