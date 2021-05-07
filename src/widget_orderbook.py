ef60314911264725f3423692757893d155fd5822
김의현 <rladml91gus@naver.com>
2021-05-07 15:34:04 +0900

    Auto stash before merge of "feature-gui" and "origin/feature-gui"

--- src/widget_orderbook.py
+++ src/widget_orderbook.py
@@ -1,44 +1,39 @@
 import sys
 import time
-import pyupbit
-import asyncio
-from component import Chart
+
+from component import Chart, Coin
 import static
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QTableWidgetItem, QProgressBar
-from PyQt5.QtCore import Qt, QThread, pyqtSignal, QPropertyAnimation
+from PyQt5.QtCore import QPropertyAnimation, Qt, QThread, pyqtSignal
 
 
 class OrderbookWorker(QThread):
-    dataSent = pyqtSignal(list)
+    dataSent = pyqtSignal(Coin)
 
     def __init__(self, ticker):
         super().__init__()
         self.size = 10
         self.ticker = ticker
         self.alive = True
+        time.sleep(1)
 
     def run(self):
         while self.alive:
-            #data = asyncio.run(pyupbit.get_orderbook("KRW-"+self.ticker))
             time.sleep(0.5)
-            if static.chart != None:
-                coin = static.chart.coins[self.ticker]
-                data = coin.get_orderbook_units()
-                if len(data) != 0:
-                    self.dataSent.emit(data)
+            if static.chart.coins["KRW-"+self.ticker] != None:
+                self.dataSent.emit(static.chart.coins["KRW-"+self.ticker])
 
     def close(self):
         self.alive = False
+
 
 class OrderbookWidget(QWidget):
     def __init__(self, parent=None, ticker="KRW-BTC"):
         super().__init__(parent)
         uic.loadUi("./src/ui/orderbook.ui", self)
-        
-        # static.chart = Chart()
-        # static.chart.sync_start()
+    
         #화면 수직,수평으로 늘릴 경우 칸에 맞게 변경됨
         #사용 가능한 공간을 채우기 위해 섹션의 크기를 자동으로 조정
         #참고 QHeaderView Class
@@ -46,11 +41,7 @@ class OrderbookWidget(QWidget):
         self.tableBids.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableAsks.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.tableBids.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        
-        #self.tableAsks.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        #self.tableBids.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-        
-        #loadUi("orderbook.ui", self)
+
         self.ticker = ticker
 
         self.asksAnim = [ ]
@@ -60,109 +51,77 @@ class OrderbookWidget(QWidget):
             item_0 = QTableWidgetItem(str(""))
             item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
             self.tableAsks.setItem(i, 0, item_0)
-
-            item_1 = QTableWidgetItem(str(""))
-            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
-            self.tableAsks.setItem(i, 1, item_1)
-
-            item_2 = QProgressBar(self.tableAsks)
-            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-            item_2.setStyleSheet("""
+ 
+            item_1 = QProgressBar(self.tableAsks)
+            item_1.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_1.setStyleSheet("""
                 QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
                 QProgressBar::Chunk {background-color : rgba(255, 0, 0, 50%);border : 1}
             """)
-            self.tableAsks.setCellWidget(i, 2, item_2)
-            
-            anim = QPropertyAnimation(item_2, b"value")
+            self.tableAsks.setCellWidget(i, 1, item_1)
+            anim = QPropertyAnimation(item_1, b"value")
             anim.setDuration(200)
             self.asksAnim.append(anim)
-            
+
             # 매수호가
             item_0 = QTableWidgetItem(str(""))
             item_0.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
             self.tableBids.setItem(i, 0, item_0)
 
-            item_1 = QTableWidgetItem(str(""))
-            item_1.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
-            self.tableBids.setItem(i, 1, item_1)
-
-            item_2 = QProgressBar(self.tableBids)
-            item_2.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-            item_2.setStyleSheet("""
+ 
+            item_1 = QProgressBar(self.tableBids)
+            item_1.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+            item_1.setStyleSheet("""
                 QProgressBar {background-color : rgba(0, 0, 0, 0%);border : 1}
                 QProgressBar::Chunk {background-color : rgba(0, 255, 0, 40%);border : 1}
             """)
-            self.tableBids.setCellWidget(i, 2, item_2)
-            anim = QPropertyAnimation(item_2, b"value")
+            self.tableBids.setCellWidget(i, 1, item_1)
+            anim = QPropertyAnimation(item_1, b"value")
             anim.setDuration(200)
             self.bidsAnim.append(anim)
+
         self.ow = OrderbookWorker(self.ticker)
         self.ow.dataSent.connect(self.updateData)
         self.ow.start()
 
-    def updateData(self, data):
-        #print(data[0]['orderbook_units'])
-        #data = data[0]['orderbook_units']
-        # 아래 코드에서 적용될 수 있게 data 형태를 바꿔줌
-        data_dict = {}
+ 
+    def updateData(self, coin):
+        data = coin.get_orderbook_units()
 
-        # 먼저 리스트로 변경하고 나중에 dict에 추가
-        asks_tmp = []
-        bids_tmp = []
-
-        for i in range(10):
-            asks_tmp.append( { 'price' : data[i]['ap'], 'quantity' : round(data[i]['as'], 5) } )
-            bids_tmp.append( { 'price' : data[i]['bp'], 'quantity' : round(data[i]['bs'], 5) } )
-        
-        data_dict['asks'] = asks_tmp
-        data_dict['bids'] = bids_tmp
-        
-        data = data_dict
-
-        # bid의 progressbar표현하기 위한 값 계산
-        bids_tradingValues = [ ]
-        asks_tradingValues = [ ]
-        for v in data['bids']:
-            bids_tradingValues.append(round(v['price'] * v['quantity'] / 10000, 3))
-        bids_maxtradingValue = max(bids_tradingValues)
-       
-        for a in data['asks'][::-1]:
-            asks_tradingValues.append(round(a['price'] * a['quantity'] / 10000, 3))
-        asks_maxtradingValue = max(asks_tradingValues)
-
-
-        #print(maxtradingValue)
-
-        for i, v in enumerate(data['asks'][::-1]):
+        #15개의 데이터를 10개로 자름
+        data = data[0:10]
+        asks_size = coin.get_total_ask_size()
+        bids_size = coin.get_total_bid_size()
+        # asks테이블 값 설정
+        for i ,v in enumerate(data[::-1]):
             item_0 = self.tableAsks.item(i, 0)
-            item_0.setText(f"{v['price']:,}")
-            item_1 = self.tableAsks.item(i, 1)
-            item_1.setText(f"{v['quantity']:,}")
-            item_2 = self.tableAsks.cellWidget(i, 2)
-            item_2.setRange(0, asks_maxtradingValue+1)
-            item_2.setFormat(f"{asks_tradingValues[i]:,}")
-            self.asksAnim[i].setStartValue(asks_tradingValues[i])
-            self.asksAnim[i].setEndValue(asks_tradingValues[i])
+            item_0.setText(f"{round(v['ap'],3):,}")
+
+            item_1 = self.tableAsks.cellWidget(i, 1)
+            # 범위가 int형으로 들어가기 때문에(값이 낮아서 전부 0으로 되는 문제)
+            # 소수 3째자리 까지 표현하므로 범위값에 1000을 곱해서 범위를 지정해줌
+            item_1.setRange(0, asks_size*1000)
+            item_1.setFormat(f"{round(v['as'],3):,}")
+            self.asksAnim[i].setStartValue(v['as']*1000)
+            self.asksAnim[i].setEndValue(v['as']*1000)
             self.asksAnim[i].start()
 
-        for i, v in enumerate(data['bids']):
+        # bids테이블 값 설정
+        for i ,v in enumerate(data):
             item_0 = self.tableBids.item(i, 0)
-            item_0.setText(f"{v['price']:,}")
-            item_1 = self.tableBids.item(i, 1)
-            item_1.setText(f"{v['quantity']:,}")
-            item_2 = self.tableBids.cellWidget(i, 2)
-            item_2.setRange(0, bids_maxtradingValue+1)
-            item_2.setFormat(f"{bids_tradingValues[i]:,}")
-            self.bidsAnim[i].setStartValue(bids_tradingValues[i])
-            self.bidsAnim[i].setEndValue(bids_tradingValues[i])
+            item_0.setText(f"{round(v['bp'],3):,}")
+           
+            item_1 = self.tableBids.cellWidget(i, 1)
+            item_1.setRange(0, bids_size*1000)
+            item_1.setFormat(f"{round(v['bs'],3):,}")
+            self.bidsAnim[i].setStartValue(v['bs']*1000)
+            self.bidsAnim[i].setEndValue(v['bs']*1000)
             self.bidsAnim[i].start()
-            
-        
 
     def closeEvent(self, event):
         self.ow.close()
 
-
+ 
 if __name__ == "__main__":
     import sys
     #from PyQt5.QtWidgets import QApplication
